420c0077761a9d8a7f73498badd6c32a938aa50f
sttk <mt_bt29s@yahoo.co.jp>
2014-12-02 06:25:26 +0900

    Add tests.

---
+++ tests/test_async_cb.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var gulp = require('gulp');
+require('../index.js');
+//require('../src/index.js');
+
+gulp.task('task0', function(end) {
+  setTimeout(end.with(function() { console.log('task0 end.'); }), 2000);
+});
+
+gulp.task('task1', function(end) {
+  setTimeout(end.with(function() { console.log('task1 end.'); }), 1000);
+});
+
+gulp.task('task2', [ 'task2.1', 'task2.2' ], function(end) {
+  setTimeout(end.with(function() { console.log('task2 end.'); }), 1000);
+});
+
+gulp.task('task2.1', function(end) {
+  setTimeout(end.with(function() { console.log('task2.1 end.'); }), 1000);
+});
+
+gulp.task('task2.2', function(end) {
+  setTimeout(end.with(function() { console.log('task2.2 end.'); }), 3000);
+});
+
+gulp.task('task3', [ 'task3.1', 'task3.2', 'task3.3' ], function(end) {
+  setTimeout(end.with(function() { console.log('task3 end.'); }), 1000);
+});
+
+gulp.task('task3.1', function(end) {
+  setTimeout(end.with(function() { console.log('task3.1 end.'); }), 3000);
+});
+
+gulp.task('task3.2', function(end) {
+  setTimeout(end.with(function() { console.log('task3.2 end.'); }), 1000);
+});
+
+gulp.task('task3.3', function(end) {
+  end(function() { console.log('task3.3 end.'); });
+});
+
+gulp.task('task4', function(end) {
+  setTimeout(end.with(function() { console.log('task4 end.'); }), 1000);
+});
+
+gulp.task('default', [ 'task0', 'task1', 'task2', 'task3', 'task4' ]);
---
+++ tests/test_async_cb.org.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var gulp = require('gulp');
+
+
+gulp.task('task0', function(cb) {
+  setTimeout(function() { console.log('task0 end.'); cb(); }, 2000);
+});
+
+gulp.task('task1', function(cb) {
+  setTimeout(function() { console.log('task1 end.'); cb(); }, 1000);
+});
+
+gulp.task('task2', [ 'task2.1', 'task2.2' ], function(cb) {
+  setTimeout(function() { console.log('task2 end.'); cb(); }, 1000);
+});
+
+gulp.task('task2.1', function(cb) {
+  setTimeout(function() { console.log('task2.1 end.'); cb(); }, 1000);
+});
+
+gulp.task('task2.2', function(cb) {
+  setTimeout(function() { console.log('task2.2 end.'); cb(); }, 3000);
+});
+
+gulp.task('task3', [ 'task3.1', 'task3.2', 'task3.3' ], function(cb) {
+  setTimeout(function() { console.log('task3 end.'); cb(); }, 1000);
+});
+
+gulp.task('task3.1', function(cb) {
+  setTimeout(function() { console.log('task3.1 end.'); cb(); }, 3000);
+});
+
+gulp.task('task3.2', function(cb) {
+  setTimeout(function() { console.log('task3.2 end.'); cb(); }, 1000);
+});
+
+gulp.task('task3.3', function(cb) {
+  console.log('task3.3 end.'); cb();
+});
+
+gulp.task('task4', function(cb) {
+  setTimeout(function() { console.log('task4 end.'); cb(); }, 1000);
+});
+
+gulp.task('default', [ 'task0', 'task1', 'task2', 'task3', 'task4' ]);
